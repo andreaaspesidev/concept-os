79271654a0b83c5770c7a21cd68dc6d0e0b6c9a8
Andrea Aspesi <aspex2014@gmail.com>
2022-09-05 00:19:20 +0200

    First version of the rcc component

---
+++ components/rcc/api/src/lib.rs
@@ -0,0 +1,203 @@
+//! A driver for the STM32F3/4 Reset and Clock Controller (RCC).
+//!
+//! This driver puts the system into a reasonable initial state, and then fields
+//! requests to alter settings on behalf of other drivers. This prevents us from
+//! needing to map the popular registers in the RCC into every driver task.
+//! 
+//! Adapted from Hubris/stm32fx-rcc
+
+#![no_std]
+
+use userlib::{hl, TaskId, FromPrimitive};
+use zerocopy::{AsBytes,FromBytes};
+
+// Import device-specific constants/structures/functions
+cfg_if::cfg_if! {
+    // STM32F3
+    if #[cfg(feature = "stm32f303re")] {
+        mod stm32f303re;
+        use stm32f303re::*;
+    }
+}
+
+/**
+ * Constants
+ */
+const RCC_TASK_ID: TaskId = TaskId(2);
+
+/**
+ * Error Type
+ */
+#[derive(Copy, Clone, Debug)]
+#[repr(u32)]
+pub enum RCCError {
+    InvalidPeripheral = 1,
+    BadArgument = 2,
+    ComponentUnavailable = 3
+}
+impl From<u32> for RCCError {
+    fn from(x: u32) -> Self {
+        match x {
+            1 => RCCError::InvalidPeripheral,
+            2 => RCCError::BadArgument,
+            _ => RCCError::ComponentUnavailable
+        }
+    }
+}
+impl From<RCCError> for u32 {
+    fn from(x: RCCError) -> Self {
+        x as u32
+    }
+}
+
+/**
+ * Operations
+ */
+#[derive(Copy, Clone, Debug, FromPrimitive)]
+pub enum Operation {
+    EnableClock = 1,
+    DisableClock = 2,
+    EnterReset = 3,
+    LeaveReset = 4
+}
+
+// Not complete, just some needed
+#[derive(Copy, Clone, Debug, FromPrimitive)]
+pub enum Peripheral {
+    TIM1 = 0,
+    TIM2 = 1,
+    TIM3 = 2,
+    TIM4 = 3,
+    TIM6 = 4,
+    TIM7 = 5,
+    TIM8 = 6,
+    WWD = 7,
+    SPI1 = 8,
+    SPI2 = 9,
+    SPI3 = 10,
+    SPI4 = 11,
+    USART1 = 12,
+    USART2 = 13,
+    USART3 = 14,
+    UART4 = 15,
+    UART5 = 16,
+    I2C1 = 17,
+    I2C2 = 18,
+    I2C3 = 19,
+    USB = 20,
+    CAN = 21,
+    DAC1 = 22,
+    DAC2 = 23,
+    PWR = 24,
+    DMA1 = 25,
+    DMA2 = 26,
+    SRAM = 27,
+    CRC = 28,
+    GPIOA = 29,
+    GPIOB = 30,
+    GPIOC = 31,
+    GPIOD = 32,
+    GPIOE = 33,
+    GPIOF = 34,
+    GPIOH = 35,
+    GPIOG = 36,
+}
+
+/// Enable clock
+#[derive(FromBytes, AsBytes)]
+#[repr(C)]
+pub struct EnableClockRequest {
+    pub bus: u32,
+    pub bit: u32
+}
+impl hl::Call for EnableClockRequest {
+    const OP: u16 = Operation::EnableClock as u16;
+    type Response = ();
+    type Err = RCCError;
+}
+
+/// Disable clock
+#[derive(FromBytes, AsBytes)]
+#[repr(C)]
+pub struct DisableClockRequest {
+    pub bus: u32,
+    pub bit: u32
+}
+impl hl::Call for DisableClockRequest {
+    const OP: u16 = Operation::DisableClock as u16;
+    type Response = ();
+    type Err = RCCError;
+}
+
+/// Enter reset
+#[derive(FromBytes, AsBytes)]
+#[repr(C)]
+pub struct EnterResetRequest {
+    pub bus: u32,
+    pub bit: u32
+}
+impl hl::Call for EnterResetRequest {
+    const OP: u16 = Operation::EnterReset as u16;
+    type Response = ();
+    type Err = RCCError;
+}
+
+/// Leave reset
+#[derive(FromBytes, AsBytes)]
+#[repr(C)]
+pub struct LeaveResetRequest {
+    pub bus: u32,
+    pub bit: u32
+}
+impl hl::Call for LeaveResetRequest {
+    const OP: u16 = Operation::LeaveReset as u16;
+    type Response = ();
+    type Err = RCCError;
+}
+
+// Bus structure
+#[derive(FromPrimitive)]
+pub enum Bus {
+    AHB1 = 0,
+    AHB2 = 1,
+    AHB3 = 2,
+    APB1 = 3,
+    APB2 = 4
+}
+
+// API Class
+pub struct RCC();
+
+impl RCC {
+    pub fn new() -> Self {
+        Self {}
+    }
+    pub fn enable_clock(&mut self, peripheral: Peripheral) -> Result<(),RCCError> {
+        let (bus, bit) = clock_mapping(peripheral)?;
+        hl::send(RCC_TASK_ID, &EnableClockRequest{
+            bus: bus as u32,
+            bit: bit
+        })
+    }
+    pub fn disable_clock(&mut self, peripheral: Peripheral) -> Result<(),RCCError> {
+        let (bus, bit) = clock_mapping(peripheral)?;
+        hl::send(RCC_TASK_ID, &DisableClockRequest{
+            bus: bus as u32,
+            bit: bit
+        })
+    }
+    pub fn enter_reset(&mut self, peripheral: Peripheral) -> Result<(),RCCError> {
+        let (bus, bit) = reset_mapping(peripheral)?;
+        hl::send(RCC_TASK_ID, &EnterResetRequest{
+            bus: bus as u32,
+            bit: bit
+        })
+    }
+    pub fn leave_reset(&mut self, peripheral: Peripheral) -> Result<(),RCCError> {
+        let (bus, bit) = reset_mapping(peripheral)?;
+        hl::send(RCC_TASK_ID, &LeaveResetRequest{
+            bus: bus as u32,
+            bit: bit
+        })
+    }
+}
---
+++ components/rcc/core/src/main.rs
@@ -0,0 +1,147 @@
+#![no_std]
+#![no_main]
+
+use rcc_api::*;
+
+// STM32F3
+#[cfg(feature = "stm32f303re")]
+use stm32f3::stm32f303 as device;
+
+use userlib::*;
+use zerocopy::AsBytes;
+
+// None of the registers we interact with have the same types, and they share no
+// useful traits, so we can't extract the bit-setting routine into a function --
+// we have no choice but to use macros.
+macro_rules! set_bits {
+    ($reg:expr, $mask:expr) => {
+        $reg.modify(|r, w| unsafe { w.bits(r.bits() | $mask) })
+    };
+}
+
+// None of the registers we interact with have the same types, and they share no
+// useful traits, so we can't extract the bit-clearing routine into a function
+// -- we have no choice but to use macros.
+macro_rules! clear_bits {
+    ($reg:expr, $mask:expr) => {
+        $reg.modify(|r, w| unsafe { w.bits(r.bits() & !$mask) })
+    };
+}
+
+#[export_name = "main"]
+fn main() -> ! {
+    // From thin air, pluck a pointer to the RCC register block.
+    //
+    // Safety: this is needlessly unsafe in the API. The RCC is essentially a
+    // static, and we access it through a & reference so aliasing is not a
+    // concern. Were it literally a static, we could just reference it.
+    let rcc = unsafe { &*device::RCC::ptr() };
+
+    // Message handler
+    let recv_handler = |op: Operation, msg: hl::Message| -> Result<(), RCCError> {
+        match op {
+            Operation::EnableClock => {
+                // Parse message (the same for all)
+                let (msg, caller) =
+                    msg.fixed::<EnableClockRequest, ()>().ok_or(RCCError::BadArgument)?;
+
+                let pmask: u32 = 1 << msg.bit;
+                let bus = Bus::from_u32(msg.bus).ok_or(RCCError::BadArgument)?;
+                // Apply
+                match bus {
+                    #[cfg(feature = "stm32f303re")]
+                    Bus::AHB1 => set_bits!(rcc.ahbenr, pmask),
+                    #[cfg(feature = "stm32f303re")]
+                    Bus::AHB2 | Bus::AHB3 => {
+                        panic!(); // The function of mapping should be correct
+                    },
+                    Bus::APB1 => set_bits!(rcc.apb1enr, pmask),
+                    Bus::APB2 => set_bits!(rcc.apb2enr, pmask),
+                };
+                // Respond
+                caller.reply(());
+                Ok(())
+            },
+            Operation::DisableClock => {
+                // Parse message (the same for all)
+                let (msg, caller) =
+                    msg.fixed::<DisableClockRequest, ()>().ok_or(RCCError::BadArgument)?;
+
+                let pmask: u32 = 1 << msg.bit;
+                let bus = Bus::from_u32(msg.bus).ok_or(RCCError::BadArgument)?;
+
+                // Apply
+                match bus {
+                    #[cfg(feature = "stm32f303re")]
+                    Bus::AHB1 => clear_bits!(rcc.ahbenr, pmask),
+                    #[cfg(feature = "stm32f303re")]
+                    Bus::AHB2 | Bus::AHB3 => {
+                        panic!(); // The function of mapping should be correct
+                    },
+                    Bus::APB1 => clear_bits!(rcc.apb1enr, pmask),
+                    Bus::APB2 => clear_bits!(rcc.apb2enr, pmask),
+                };
+                // Respond
+                caller.reply(());
+                Ok(())
+            },
+            Operation::EnterReset => {
+                // Parse message (the same for all)
+                let (msg, caller) =
+                    msg.fixed::<EnterResetRequest, ()>().ok_or(RCCError::BadArgument)?;
+
+                let pmask: u32 = 1 << msg.bit;
+                let bus = Bus::from_u32(msg.bus).ok_or(RCCError::BadArgument)?;
+
+                // Apply
+                match bus {
+                    #[cfg(feature = "stm32f303re")]
+                    Bus::AHB1 => set_bits!(rcc.ahbrstr, pmask),
+                    #[cfg(feature = "stm32f303re")]
+                    Bus::AHB2 | Bus::AHB3 => {
+                        panic!(); // The function of mapping should be correct
+                    },
+                    Bus::APB1 => set_bits!(rcc.apb1rstr, pmask),
+                    Bus::APB2 => set_bits!(rcc.apb2rstr, pmask),
+                };
+                // Respond
+                caller.reply(());
+                Ok(())
+            },
+            Operation::LeaveReset => {
+                // Parse message (the same for all)
+                let (msg, caller) =
+                    msg.fixed::<LeaveResetRequest, ()>().ok_or(RCCError::BadArgument)?;
+
+                let pmask: u32 = 1 << msg.bit;
+                let bus = Bus::from_u32(msg.bus).ok_or(RCCError::BadArgument)?;
+
+                // Apply
+                match bus {
+                    #[cfg(feature = "stm32f303re")]
+                    Bus::AHB1 => clear_bits!(rcc.ahbrstr, pmask),
+                    #[cfg(feature = "stm32f303re")]
+                    Bus::AHB2 | Bus::AHB3 => {
+                        panic!(); // The function of mapping should be correct
+                    },
+                    Bus::APB1 => clear_bits!(rcc.apb1rstr, pmask),
+                    Bus::APB2 => clear_bits!(rcc.apb2rstr, pmask),
+                };
+                // Respond
+                caller.reply(());
+                Ok(())
+            }
+        }
+    };
+
+    // Incoming message buffer
+    // Ensure our buffer is aligned properly for two u32 by declaring it as one.
+    let mut buffer = [0u32; 2];
+
+    // Main loop
+    loop {
+        // Wait for a command
+        // TODO: implement notification as it could be a message from the OS
+        hl::recv_without_notification(buffer.as_bytes_mut(), recv_handler);
+    }
+}
