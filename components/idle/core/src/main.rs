baef8422cc954f43c7c8d54804eaadabd77d0b90
Andrea Aspesi <aspex2014@gmail.com>
2022-10-11 16:53:35 +0200

    Added missing kipc call

--- components/idle/core/src/main.rs
+++ components/idle/core/src/main.rs
@@ -1,12 +1,11 @@
 #![no_std]
 #![no_main]
 
-// Make sure we actually link in userlib, despite not using any of it explicitly
-// - we need it for our _start routine.
-extern crate userlib;
+use userlib::*;
 
 #[export_name = "main"]
 fn main() -> ! {
+    kipc::activate_task();
     loop {
         // Wait For Interrupt to pause the processor until an ISR arrives,
         // which could wake some higher-priority task.
